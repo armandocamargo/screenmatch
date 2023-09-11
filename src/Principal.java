93a1d890cab265a36e85aa8fc1adbff62a015bfa
Armando Camargo <armando_margo@hotmail.com>
2023-09-10 23:33:51 -0300

    implementando getters e setters

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,12 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.durcaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDurcaoEmMinutos(180);
+
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
